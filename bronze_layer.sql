0ed2a47f5f2f118c607de06fc9c8074a9f23d7b4
Vigneshkumarcvk <117513448+Vigneshkumarcvk@users.noreply.github.com>
2026-03-29 19:12:18 +0530

    Delete bronze_layer.sql

--- bronze_layer.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TABLE bronze_sales_all as SELECT * FROM workspace.default.adventure_works_sales_2015 UNION ALL SELECT * FROM workspace.default.adventure_works_sales_2016 UNION ALL SELECT * FROM workspace.default.adventure_works_sales_2017;
-
-
-select * from bronze_sales_all;
-
-
-SELECT Count(*) FROM bronze_sales_all;
-
-select min(OrderDate), max(OrderDate) from bronze_sales_all;
